9d94943599c157573a03237d46f0c1de40f2da61
vanamahesh <vanamahesh@deloitte.com>
2025-04-07 02:14:23 +0530

    exploring get, post delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("hello from the dashboard");
-});
+// app.use("/", (req, res) => {
+//     res.send("hello from the dashboard");
+// });
 
 app.use("/test", (req, res) => {
     res.send("hello from the server");
 });
 
-app.use("/hello", (req,res)=>{
-    res.send("hello hello  ");
+// app.use("/user", (req, res) => {
+//     res.send("hahahah");
+// });
+
+app.get("/user", (req, res) => {
+    res.send({ firstName: "mahesh", lastName: "vanam"});
+});
+
+app.post("/user", (req, res) => {
+    res.send("updated user in the database");
 });
 
 app.listen(3000, ()=> {
