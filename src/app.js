427e37d73d466ee5055e1461d946baeaf33b478d
vanamahesh <vanamahesh@deloitte.com>
2025-04-07 02:36:02 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,16 @@ const express = require('express');
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//     res.send("hello from the dashboard");
-// });
 
-app.use("/test", (req, res) => {
-    res.send("hello from the server");
-});
-
-// app.use("/user", (req, res) => {
-//     res.send("hahahah");
-// });
 
 app.get("/user", (req, res) => {
+    console.log(req.query);
     res.send({ firstName: "mahesh", lastName: "vanam"});
 });
 
-app.post("/user", (req, res) => {
-    res.send("updated user in the database");
+app.get("/user/:userID/:username/:password", (req, res) =>{
+    console.log(req.params);
+    res.send({firstName: "mahesh", lastName: "vanam"});
 });
 
 app.listen(3000, ()=> {
