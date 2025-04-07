ceaff6c70ccd4798106aaa488ab9c28200733bef
vanamahesh <vanamahesh@deloitte.com>
2025-04-07 22:09:01 +0530

    middle wares and route handlers

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user",
+(req, res, next) => {
+    console.log("handling the route 1");
+    // res.send("response");
+    next();
+},
 
+(req,res,next) => {
+    // res.send("response2");
+    next();
+},
 
-app.get("/user", (req, res) => {
-    console.log(req.query);
-    res.send({ firstName: "mahesh", lastName: "vanam"});
-});
-
-app.get("/user/:userID/:username/:password", (req, res) =>{
-    console.log(req.params);
-    res.send({firstName: "mahesh", lastName: "vanam"});
+(req,res) => {
+    res.send("response3");
 });
 
 app.listen(3000, ()=> {
     console.log("server is listening on port 3000...");
 });
+  
