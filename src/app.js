0b0f891e899e29569bb89bbb25c25ee30d888ee2
vanamahesh <vanamahesh@deloitte.com>
2025-04-08 17:48:48 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,23 +1,35 @@
 const express = require('express');
-const app = express()
 
-app.get('/getuserdata', (req, res)=>{
+const {connectDB} = require("./config/database");
+const {User} = require("./models/user")
+const app = express();
+
+app.post('/signup', async(req,res)=>{
+    const user = new User({
+        firstName:"virat", 
+        lastName: "kohli", 
+        emailId:"virat@kohli",
+        password: "vkohli@123",
+    });
+    
+    // returns promise and inserts data into the database
     try{
-        throw new Error("asfsf");
-        res.send("user data sent");
+        await user.save();
+        res.send("user added successfully");
     }
     catch(err){
-        res.status(500).send("some error contact support team");
+        res.status(400).send("error saving the user: ", err.message);
     }
 });
 
-app.use('/', (err, req, res, next) =>{
-    if(err){
-        res.status(500).send("something went wrong");
-    }
+connectDB()
+.then(() =>{
+    console.log("database connection established..");
+    app.listen(3000, ()=> {
+        console.log("server is listening on port 3000...");
+    });
+})
+.catch((err) => {
+    console.error("database cannot be connected");
 });
 
-app.listen(3000, ()=> {
-    console.log("server is listening on port 3000...");
-});
-  
